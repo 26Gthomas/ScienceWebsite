56888909b6a1b9e1b6ec431f8894cab3b49670a8
26Gthomas <91628131+26Gthomas@users.noreply.github.com>
2021-11-03 18:17:40 -0400

    Create PollutionGraph.js

---
+++ PollutionGraph.js
@@ -0,0 +1,35 @@
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'X');
+      data.addColumn('number', 'Landfilled');
+      data.addColumn('number', 'Recycled');
+      data.addColumn('number', 'Combustion with Energy Recoverd');
+
+      data.addRows([
+        ['1960', 390000, 0, 0], ['1970', 2900000, 0, 0], ['1980', 6670000, 20000, 140000], ['1990', 13780000, 370000, 2980000], ['2000', 19950000, 1480000, 4120000], ['2005', 23270000, 1780000, 4330000], ['2010', 24370000, 2500000, 4530000], ['2015', 26030000, 3120000, 5330000], ['2017', 26820000, 3000000, 5590000], ['2018', 26970000, 3090000, 5620000]
+      ]);
+
+      var options = {
+        hAxis: {
+          title: 'Year',
+          format: '####'
+        },
+        vAxis: {
+          title: 'Tons'
+        },
+        width: 900,
+        height: 500,
+        gridlines: {
+        	minSpacing: 1,
+        	interval: [1],
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
