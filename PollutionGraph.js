ecfdf9c9cebe0fcc8902e7215236845b8f89c91d
26Gthomas <91628131+26Gthomas@users.noreply.github.com>
2021-11-03 18:56:26 -0400

    Update PollutionGraph.js

--- PollutionGraph.js
+++ PollutionGraph.js
@@ -12,7 +12,7 @@ function drawBasic() {
       data.addColumn('string', 'X');
       data.addColumn('number', 'Landfilled');
       data.addColumn('number', 'Recycled');
-      data.addColumn('number', 'Combustion with Energy Recoverd');
+      data.addColumn('number', 'Combustion with Energy Recovered');
 
       data.addRows([
         ['1960', 390000, 0, 0], ['1970', 2900000, 0, 0], ['1980', 6670000, 20000, 140000], ['1990', 13780000, 370000, 2980000], ['2000', 19950000, 1480000, 4120000], ['2005', 23270000, 1780000, 4330000], ['2010', 24370000, 2500000, 4530000], ['2015', 26030000, 3120000, 5330000], ['2017', 26820000, 3000000, 5590000], ['2018', 26970000, 3090000, 5620000]
@@ -31,7 +31,7 @@ function drawBasic() {
             actions: ['dragToZoom', 'rightClickToReset']
         },
         legend: {
-            position: bottom,
+            position: 'bottom',
         }      
       };
 
